487781f21f0729345e5a4e48feff3edcc95acd29
Commander Coder <commandercoder.2010@gmail.com>
2020-06-24 08:31:59 +0100

    Initial Commit

---
+++ BeebEm5/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  BeebEm5
+//
+//  Created by Commander Coder on 24/06/2020.
+//  Copyright © 2020 Andrew Hague. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
