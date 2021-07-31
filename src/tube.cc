63a1f819774f66021fbc8577ae57e9c8a688ef3f
Steve Inglis <sai2791@googlemail.com>
2021-07-31 15:41:16 +0100

    More updates to tube.cc

--- src/tube.cc
+++ src/tube.cc
@@ -95,31 +95,31 @@ static bool TubeNMILock = false; // Well I think NMI's are maskable - to stop re
 typedef void (*InstrHandlerFuncType)(int16 Operand);
 typedef int16 (*AddrModeHandlerFuncType)(int WantsAddr);
 
-static int TubeCyclesTable[]={
-  7,6,0,0,0,3,5,5,3,2,2,0,0,4,6,0, /* 0 */
-  2,5,0,0,0,4,6,0,2,4,0,0,0,4,7,0, /* 1 */
-  6,6,0,0,3,3,5,0,4,2,2,0,4,4,6,0, /* 2 */
-  2,5,0,0,0,4,6,0,2,4,0,0,0,4,7,0, /* 3 */
-  6,6,0,0,0,3,5,0,3,2,2,2,3,4,6,0, /* 4 */
-  2,5,0,0,0,4,6,0,2,4,0,0,0,4,7,0, /* 5 */
-  6,6,0,0,0,3,5,0,4,2,2,0,5,4,6,0, /* 6 */
-  2,5,0,0,0,4,6,0,2,4,0,0,0,4,7,0, /* 7 */
-  2,6,0,0,3,3,3,3,2,0,2,0,4,4,4,0, /* 8 */
-  2,6,0,0,4,4,4,0,2,5,2,0,0,5,0,0, /* 9 */
-  2,6,2,0,3,3,3,0,2,2,2,0,4,4,4,0, /* a */
-  2,5,0,0,4,4,4,0,2,4,2,0,4,4,4,0, /* b */
-  2,6,0,0,3,3,5,0,2,2,2,0,4,4,6,0, /* c */
-  2,5,0,0,0,4,6,0,2,4,0,0,4,4,7,0, /* d */
-  2,6,0,0,3,3,5,0,2,2,2,0,4,4,6,0, /* e */
-  2,5,0,0,0,4,6,0,2,4,0,0,0,4,7,0  /* f */
-}; /* TubeCyclesTable */
+static const int TubeCyclesTable[] = {
+/*0 1 2 3 4 5 6 7 8 9 a b c d e f */
+  7,6,2,1,5,3,5,5,3,2,2,1,6,4,6,5, /* 0 */
+  2,5,5,1,5,4,6,5,2,4,2,1,6,4,6,5, /* 1 */
+  6,6,2,1,3,3,5,5,4,2,2,1,4,4,6,5, /* 2 */
+  2,5,5,1,4,4,6,5,2,4,2,1,4,4,6,5, /* 3 */
+  6,6,2,1,3,3,5,5,3,2,2,1,3,4,6,5, /* 4 */
+  2,5,5,1,4,4,6,5,2,4,3,1,8,4,6,5, /* 5 */
+  6,6,2,1,3,3,5,5,4,2,2,1,6,4,6,5, /* 6 */
+  2,5,5,1,4,4,6,5,2,4,4,1,6,4,6,5, /* 7 */
+  3,6,2,1,3,3,3,5,2,2,2,1,4,4,4,5, /* 8 */
+  2,6,5,1,4,4,4,5,2,5,2,1,4,5,5,5, /* 9 */
+  2,6,2,1,3,3,3,5,2,2,2,1,4,4,4,5, /* a */
+  2,5,5,1,4,4,4,5,2,4,2,1,4,4,4,5, /* b */
+  2,6,2,1,3,3,5,5,2,2,2,1,4,4,6,5, /* c */
+  2,5,5,1,4,4,6,5,2,4,3,1,4,4,7,5, /* d */
+  2,6,2,1,3,3,5,5,2,2,2,1,4,4,6,5, /* e */
+  2,5,5,1,4,4,6,5,2,4,4,1,4,4,7,5  /* f */
+};
 
 /* The number of TubeCycles to be used by the current instruction - exported to
    allow fernangling by memory subsystem */
 unsigned int TubeCycles;
 
-static bool Branched;
-// Branched - true if the instruction branched
+static bool Branched; // Branched - true if the instruction branched
 
 /* A macro to speed up writes - uses a local variable called 'tmpaddr' */
 #define TUBEREADMEM_FAST(a) ((a<0xfef8)?TubeRam[a]:TubeReadMem(a))
@@ -128,7 +128,7 @@ static bool Branched;
 #define TUBEFASTWRITE(addr,val) tmpaddr=addr; if (tmpaddr<0xfef8) TUBEWRITEMEM_DIRECT(tmpaddr,val) else TubeWriteMem(tmpaddr,val);
 
 // Local fns
-void Reset65C02(void);
+static void Reset65C02(void);
 void ResetTube(void);
 
 // Staus bits
