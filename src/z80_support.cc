85c52f6482299afdde26cb118fea2c1a956a1982
Steve Inglis <sai2791@googlemail.com>
2021-08-08 18:30:05 +0100

    Revert "Update z80_support to be more like the windows version"
    This reverts commit df20250f517d2aec4fbf9801117582294572f8be.

--- src/z80_support.cc
+++ src/z80_support.cc
@@ -1,64 +1,50 @@
-/****************************************************************
-BeebEm - BBC Micro and Master 128 Emulator
-Copyright (C) 2005  Jon Welch
-
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
-of the License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public
-License along with this program; if not, write to the Free
-Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-Boston, MA  02110-1301, USA.
-****************************************************************/
-
 #include <stdio.h>
 #include <string.h>
 
-// include "log.h"
 #include "main.h"
 #include "debug.h"
 #include "z80mem.h"
 #include "z80.h"
 #include "beebmem.h"
+
 #include "tube.h"
 
 extern int trace;
 
 int Enable_Z80 = 0;
-bool trace_z80 = false;
-int PreZPC = 0; // Previous Z80 PC
-
+int trace_z80 = 0;
 int debug_z80 = 0;
 int TorchTube = 0;
 
 unsigned char z80_rom[65536L];
 unsigned char z80_ram[65536L];
 
-bool inROM = true;
+#define TRUE 1
+#define FALSE 0
 
-unsigned char ReadZ80Mem(int addr)
+int inROM = 1;
+
+unsigned char ReadZ80Mem(int pc)
+
 {
-	if (TubeType == Tube::AcornZ80)
+unsigned char t;
+
+	if (AcornZ80)
 	{
-		if (addr >= 0x8000) inROM = false;
+		if (pc >= 0x8000) inROM = 0;
 	}
 	
-    unsigned char t = (inROM) ? z80_rom[addr & 0x1fff] : z80_ram[addr & 0xffff];
+    t = (inROM) ? z80_rom[pc & 0xffff] : z80_ram[pc & 0xffff];
 
 //    if (trace_z80)
-//        WriteLog("Read %02x from %04x in %s\n", t, addr, (inROM) ? "ROM" : "RAM");
+//        WriteLog("Read %02x from %04x in %s\n", t, pc, (inROM) ? "ROM" : "RAM");
 
     return t;
+
 }
 
-void WriteZ80Mem(int addr, unsigned char data)
+void WriteZ80Mem(int pc, unsigned char data)
+
 {
 //    if (inROM)
 //    {
@@ -66,11 +52,12 @@ void WriteZ80Mem(int addr, unsigned char data)
 //    }
 //    else
 //    {
-        z80_ram[addr & 0xffff] = data;
+        z80_ram[pc & 0xffff] = data;
 //    }
 
 //    if (trace_z80)
-//        WriteLog("Writing %02x to %04x\n", data, addr);
+//        WriteLog("Writing %02x to %04x\n", data, pc);
+
 }
 
 /* 
@@ -80,6 +67,7 @@ AF'0000 MZ5H3VNC BC'0000 DE'0000 HL'0000 IY=0000 R=00 SP=0000:00,00,00,00
  */
 
 void Disp_RegSet1(char *str)
+
 {
 	sprintf(str, "AF=%04X ",af[0]);
 	sprintf(str + strlen(str), (af[0] & 128) ? "M" : "P");
@@ -96,6 +84,7 @@ void Disp_RegSet1(char *str)
 }
 
 void Disp_RegSet2(char *str)
+
 {
 	sprintf(str, "AF'%04X ",af[1]);
 	sprintf(str + strlen(str), (af[1] & 128) ? "M" : "P");
@@ -114,6 +103,7 @@ void Disp_RegSet2(char *str)
 
 void disp_regs()
 {
+
 char buff[64];
 char str[256];
 	
@@ -148,47 +138,55 @@ sprintf(str + strlen(str), " IY=%04X R=%02x SP=%04X",iy, regs_sel, sp);
 sprintf(str + strlen(str), ":%02X,%02X,%02X,%02X\n",ReadZ80Mem(sp),ReadZ80Mem(sp+1),ReadZ80Mem(sp+2),ReadZ80Mem(sp+3));
 
 WriteLog("%s\n", str);
+
 }
 
 unsigned char in(unsigned int addr)
+
 {
-unsigned char value = 0xff;
+int value = 0xff;
+int tmp;
+// int c;
 
-    addr &= 0xff;
+    addr &= 255;
 
-	if (TubeType == Tube::AcornZ80)
+	if (AcornZ80)
 	{
-        value = ReadTubeFromParasiteSide((unsigned char)addr);
+        value = ReadTubeFromParasiteSide(addr);
 	}
 	else
 	{
-		if (addr == 0x05 || addr == 0x01)
+		if ( (addr == 0x05) || (addr == 0x01) )
 		{
 			value = ReadTorchTubeFromParasiteSide(1);        // Data Port
+
 		}
 
-		if (addr == 0x06 || addr == 0x02)
+		if ( (addr == 0x06) || (addr == 0x02) )
 		{
 			value = ReadTorchTubeFromParasiteSide(0);        // Status Port
-			unsigned char tmp = 0x00;
+			tmp = 0x00;
 			if (value & 128) tmp |= 2;      // Tube data available
 			if (value & 64) tmp |= 128;     // Tube not full
 			value = tmp;
 		}
-        
-        inROM = (addr & 4) == 0;
+
+		if (addr == 0x02) inROM = 1;
+
+		if (addr == 0x06) inROM = 0;
     }
 	
     return value;
 }
 
 void out(unsigned int addr, unsigned char value)
+
 {
     addr &= 255;
     
-	if (TubeType == Tube::AcornZ80)
+	if (AcornZ80)
 	{
-        WriteTubeFromParasiteSide((unsigned char)addr, value);
+        WriteTubeFromParasiteSide(addr, value);
 	}
 	else
 	{
@@ -196,157 +194,147 @@ void out(unsigned int addr, unsigned char value)
 		{
 			WriteTorchTubeFromParasiteSide(1, value);
 		}
-
-        inROM = (addr & 4) == 0;
 	}
 }
 
 void z80_execute()
 {
-    if (trace_z80)
+    if (Enable_Z80)
     {
-        if (pc <= 0xf800)       // Don't trace BIOS cos toooo much data
-            disp_regs();
-    }
 
-    // Output debug info
-    if (DebugEnabled)
-        DebugDisassembler(pc, 0, 0, 0, 0, false);
-    // DebugDisassembler(pc, PreZPC, 0, 0, 0, 0, 0, false);
+        if (trace_z80)
+        {
+            if (pc <= 0xf800)       // Don't trace BIOS cos toooo much data
+                disp_regs();
+        }
 
-    PreZPC = pc;
-    pc = (WORD)simz80(pc);
+		// Output debug info
+		if (DebugEnabled)
+			DebugDisassembler(pc, 0, 0, 0, 0, false);
+		
+		pc = simz80(pc);
 
-    if (TubeType == Tube::AcornZ80)
-    {
-        if (TubeintStatus & (1<<R1))
-            set_Z80_irq_line(true);
-
-        if (TubeintStatus & (1<<R4))
-            set_Z80_irq_line(true);
-
-        if (TubeintStatus == 0)
-            set_Z80_irq_line(false);
-
-        if (TubeNMIStatus) 
-            set_Z80_nmi_line(true);
-        else
-            set_Z80_nmi_line(false);
-    }
+		if (AcornZ80)
+		{
+			if (TubeintStatus & (1<<R1))
+				set_Z80_irq_line(1);
+		
+			if (TubeintStatus & (1<<R4))
+				set_Z80_irq_line(1);
+		
+			if (TubeintStatus == 0)
+				set_Z80_irq_line(0);
+		
+			if (TubeNMIStatus) 
+				set_Z80_nmi_line(1);
+			else
+				set_Z80_nmi_line(0);
+		}
+	}
 }
 
 void init_z80()
+
 {
-    char path[256];
+char path[256];
+FILE *f;
 
     WriteLog("init_z80()\n");
 
-    if (TubeType == Tube::AcornZ80)
-    {
-        strcpy(path, RomPath);
-        strcat(path, "/BeebFile/Z80.ROM");
+	if (AcornZ80)
+	{
+		strcpy(path, RomPath);
+		strcat(path, "/BeebFile/Z80.ROM");
+		
+		f = fopen(path, "rb");
+		if (f != NULL)
+		{
+			fread(z80_rom, 4096, 1, f);
+			fclose(f);
+		}
+	}
+	else
+	{
+		strcpy(path, RomPath);
+		strcat(path, "/BeebFile/CCPN102.ROM");
 
-        FILE *f = fopen(path, "rb");
-        if (f != NULL)
-        {
-            fread(z80_rom, 4096, 1, f);
-            fclose(f);
-        }
-    }
-    else // Tube::TorchZ80
-    {
-        strcpy(path, RomPath);
-        strcat(path, "/BeebFile/CCPN102.ROM");
-
-        FILE *f = fopen(path, "rb");
-        if (f != NULL)
-        {
-            size_t addr = 0;
-            fseek(f, 0, SEEK_END);
-            long count = ftell(f);
-            if (count > 4096) {
-                fseek(f, 0, SEEK_SET);
-                addr=addr+fread(z80_rom+0, 8192, 1, f);
-            } else {
-                if (count > 2048) {
-                    fseek(f, 0, SEEK_SET);
-                    addr=addr+fread(z80_rom+0, 4096, 1, f);
-                    fseek(f, 0, SEEK_SET);
-                    addr=addr+fread(z80_rom+4096, 4096, 1, f);
-                } else {
-                    fseek(f, 0, SEEK_SET);
-                    addr=addr+fread(z80_rom+0, 2048, 1, f);
-                    fseek(f, 0, SEEK_SET);
-                    addr=addr+fread(z80_rom+2048, 2048, 1, f);
-                    fseek(f, 0, SEEK_SET);
-                    addr=addr+fread(z80_rom+4096, 2048, 1, f);
-                    fseek(f, 0, SEEK_SET);
-                    addr=addr+fread(z80_rom+6144, 2048, 1, f);
-                }
-            }
-            fclose(f);
-        }
-    }
-
-    inROM = true;
+		f = fopen(path, "rb");
+		if (f != NULL)
+		{
+			fread(z80_rom, 8192, 1, f);
+			fclose(f);
+		}
+	}
+	
+    inROM = 1;
 
     sp = 0x00;
     pc = 0x00;
-
-    // Clear all registers, PC and SP have already been set
+    
+/* Clear all registers, PC and SP have already been set			*/
     af[0]=0; regs[0].bc=0; regs[0].de=0; regs[0].hl=0;
     af[1]=0; regs[1].bc=0; regs[1].de=0; regs[1].hl=0;
     ix=0; iy=0; ir=0; regs_sel = 0;
+
 }
 
 void Debug_Z80()
+
 {
 char buff[256];
+int s, t, a;
 
-int t = 0x4400;
 
-    for (int a = 0; a < 512; ++a)
+    t = 0x4400;
+    for (a = 0; a < 512; ++a)
     {
-        int s = Z80_Disassemble(t, buff);
+        s = Z80_Disassemble(t, buff);
         WriteLog("%04x : %s\n", t, buff);
         t += s;
     }
             
-    trace_z80 = true;
+    trace_z80 = 1;
 
-    for (int a = 0; a < 32; ++a)
+    for (a = 0; a < 32; ++a)
     {
         PrintHex(0x4400 + a * 16);
     }
+
 }
 
 void PrintHex(int addr)
+
 {
-    char buff[80];
+char buff[80];
+int i, a;
+int num;
+char *p;
 
-    const int num = 16;
+	num = 16;
 
-    char *p = buff;
-    sprintf(p, "%04X : ", addr);
-    p += 7;
+	p = buff;
+	sprintf(p, "%04X : ", addr);
+	p += 7;
 
-    for (int i = 0; i < num; ++i)
-    {
-        sprintf(p, "%02X ", ReadZ80Mem(addr + i));
-        p += 3;
-    }
+	for (i = 0; i < num; ++i)
+	{
+		sprintf(p, "%02X ", ReadZ80Mem(addr + i));
+		p += 3;
+	}
 
-    strcpy(p, " ");
-    p += 1;
+	strcpy(p, " ");
+	p += 1;
 
-    for (int i = 0; i < num; ++i)
-    {
-        int a = ReadZ80Mem(addr + i) & 127;
-        if (a < 32 || a == 127) a = '.';
-        if (a == '%') a = '.';
-        *p++ = (char)a;
+	for (i = 0; i < num; ++i)
+	{
+		a = ReadZ80Mem(addr + i) & 127;
+		if (a < 32 || a == 127) a = '.';
+		if (a == '%') a = '.';
+		*p++ = a;
         *p = 0;
     }
 
     WriteLog("%s\n", buff);
+		
 }
+
