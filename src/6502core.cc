d35b1d8f1c517963fdb693f86664d46732cf18cf
Steve Inglis <sai2791@googlemail.com>
2022-08-27 10:40:07 +0100

    Update to 6502 core to fix some of the cycle counts for instructions taken from the windows version of BeebEm

--- src/6502core.cc
+++ src/6502core.cc
@@ -29,7 +29,9 @@
 #include "beebmem.h"
 #include "beebsound.h"
 // #include "music5000.h"
+#include "beebwin.h"
 #include "disc8271.h"
+#include "model.h"
 #include "via.h"
 #include "sysvia.h"
 #include "uservia.h"
@@ -113,7 +115,7 @@ static int CyclesTable6502[]={
   2,5,1,8,4,4,6,6,2,4,2,7,4,4,7,7, /* 5 */
   6,6,1,8,3,3,5,5,4,2,2,2,5,4,6,6, /* 6 */
   2,5,1,8,4,4,6,6,2,4,2,7,4,4,7,7, /* 7 */
-  2,6,2,6,3,3,3,3,2,2,2,2,4,4,4,4, /* 8 */
+  2,6,2,6,3,3,3,3,2,2,2,2,4,4,4,7, /* 8 */
   2,6,1,6,4,4,4,4,2,5,2,5,5,5,5,5, /* 9 */
   2,6,2,6,3,3,3,3,2,2,2,2,4,4,4,4, /* a */
   2,5,1,5,4,4,4,4,2,4,2,4,4,4,4,4, /* b */
@@ -133,7 +135,7 @@ static const int CyclesTable65C02[] = {
   2,5,5,1,4,4,6,1,2,4,3,1,8,4,6,1, /* 5 */
   6,6,2,1,3,3,5,1,4,2,2,1,6,4,6,1, /* 6 */
   2,5,5,1,4,4,6,1,2,4,4,1,6,4,6,1, /* 7 */
-  3,6,2,1,3,3,3,1,2,2,2,1,4,4,4,1, /* 8 */
+  2,6,2,1,3,3,3,1,2,2,2,1,4,4,4,1, /* 8 */
   2,6,5,1,4,4,4,1,2,5,2,1,4,5,5,1, /* 9 */
   2,6,2,1,3,3,3,1,2,2,2,1,4,4,4,1, /* a */
   2,5,5,1,4,4,4,1,2,4,2,1,4,4,4,1, /* b */
@@ -268,8 +270,22 @@ static void PollHardware(unsigned int nCycles);
 static unsigned int InstructionCount[256];
 
 /*----------------------------------------------------------------------------*/
-INLINE void Carried() {
-    // Correct cycle count for indirection across page boundary
+
+// Correct cycle count for indirection across page boundary
+
+static INLINE void Carried() 
+{
+    if (MachineType == Model::Master128)
+    {
+        if (CurrentInstruction == 0x1e ||
+            CurrentInstruction == 0x3e ||
+            CurrentInstruction == 0x5e ||
+            CurrentInstruction == 0x7e)
+        {
+            Cycles++;
+        }
+    }
+
     if (((CurrentInstruction & 0xf)==0x1 ||
          (CurrentInstruction & 0xf)==0x9 ||
          (CurrentInstruction & 0xf)==0xd) &&
